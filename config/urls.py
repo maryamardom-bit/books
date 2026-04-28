5d0ad2ea086cc3720185a8fa7e49137bb8879da5
unknown <you@example.com>
2026-04-28 13:51:30 +0330

    create product_list

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
+    path('products' , include('products.urls')),
+
 ]
