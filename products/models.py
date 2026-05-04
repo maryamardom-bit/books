80c3d940ad069ef63fdbc967a514f3325055cd32
unknown <you@example.com>
2026-05-04 14:19:03 +0330

    product/forms.py

--- products/models.py
+++ products/models.py
@@ -37,6 +37,10 @@ class Comment(models.Model):
 
     active = models.BooleanField(default= True)
 
+    def get_absolute_url(self):
+        return reverse('product_detail', args= [self.product.id])
+    
+
 
 
 
