380765dcd69bd7274c836d897b76361e9975d8df
unknown <you@example.com>
2026-05-05 13:31:14 +0330

    add templatetags

--- products/admin.py
+++ products/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 
 from .models import Product,Comment
 
+class CommentsInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author','body' , 'stars' , 'active',]
+    extra = 0
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'price' , 'active']
+    list_display = ['title' , 'price' , 'active',]
 
+    inlines =[
+        CommentsInLine ,
+    ]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['product' , 'author','body' , 'stars' , 'active']
+    list_display = ['product' , 'author','body' , 'stars' , 'active',]
 
--- products/models.py
+++ products/models.py
@@ -17,25 +17,41 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('product_detail', args = [self.pk])
-    
+
+
+
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active = True)
+
 class Comment(models.Model):
     PRODUCT_STARS = [
         (1 , 'Very Bad'),
         (2 , 'Bad'), 
         (3 , 'Normal'), 
         (4 , 'Good'), 
-        (5, 'Very Good'),  
+        (5, 'Perfect'),  
     ]
 
     product = models.ForeignKey(Product ,on_delete= models.CASCADE , related_name= 'comments', )
-    author =models.ForeignKey(get_user_model(), on_delete= models.CASCADE , related_name= 'comments',)
-    body = models.TextField()
-    stars = models.IntegerField(choices=PRODUCT_STARS)
+    author =models.ForeignKey(
+        get_user_model(),
+        on_delete= models.CASCADE ,
+        related_name= 'comments',
+        verbose_name= 'comment_author',
+        )
+        
+    body = models.TextField(verbose_name='Comment Text')
+    stars = models.IntegerField(choices=PRODUCT_STARS, verbose_name= 'What is your score?')
 
     datetime_created = models.DateTimeField(auto_now_add= True)
     detetime_modified = models.DateTimeField(auto_now= True)
 
     active = models.BooleanField(default= True)
+
+    # manager
+    objects = models.Manager()
+    active_comments_manager = ActiveCommentsManager()
 
     def get_absolute_url(self):
         return reverse('product_detail', args= [self.product.id])
