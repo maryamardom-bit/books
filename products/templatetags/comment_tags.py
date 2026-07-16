68bf812076198e45f016a564f1464c95ecdc68d4
unknown <you@example.com>
2026-07-16 23:00:17 +0330

    chane footer

--- products/templatetags/comment_tags.py
+++ products/templatetags/comment_tags.py
@@ -7,3 +7,10 @@ def only_active_comments(comments):
     return comments.filter(active = True).order_by("-datetime_created")
 
 
+
+@register.filter
+def average_rating(comments):
+    if not comments:
+        return 0
+    total = sum(comment.stars for comment in comments)
+    return round(total / len(comments), 1)
